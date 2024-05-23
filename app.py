70af92a6d80407ca949c24aaaae460c69d2c8333
Nora-Kasiem <93841424+Nora-Kasiem@users.noreply.github.com>
2024-05-23 07:16:24 +0000

    Updates

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 from st_audiorec import st_audiorec
-from transformers import pipeline
-from keras.models import load_model
+import tensorflow as tf
 import numpy as np
-# from skimage import transform
-from PIL import Image
+from PIL import Image, ImageOps
 from transformers import pipeline
-
+import cv2
 
 # Page Config
 st.set_page_config(page_title="Deafon", page_icon="logo.png", layout="wide")
@@ -69,15 +67,28 @@ def translating(text, src, dest):
     output = translator(text)[0]['translation_text']
     return output
 
-@st.cache_resource
-def loading():
-    model = load_model("arsl_cnn_model.h5")
+
+def import_and_predict(image_data, model):
+        size = (224, 224)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)       
+        img_reshape = img[np.newaxis,...]
+        prediction = model.predict(img_reshape)
+        return prediction
+
+
+@st.cache_data()
+def run_model(lang):
+    model = tf.keras.models.load_model(f'./{lang}-model.h5')
     return model
+
 
 @st.cache_resource
 def audio_text():
     pipe = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3")
     return pipe
+
 
 types = ["Text", "Microphone", "Camera"]
 t1, t2, t3 = st.tabs(types)
@@ -86,48 +97,47 @@ with t1: # Text Input
     intext = st.text_area("Type your text here", key="input_text")
     
     if intext is not None:
-        with st.chat_message("assistant"):
-            temp = translating(intext, src, dest)
-            st.markdown(temp)
+        # with st.chat_message("assistant"):
+        temp = translating(intext, src, dest)
+        st.markdown(temp)
 
 
 with t2: # Audio Input
     wav_audio_data = st_audiorec()
+    
+    if wav_audio_data is not None:
+        # with st.chat_message("assistant"):
+        pipe = audio_text()
+        # autext = pipe(wav_audio_data, generate_kwargs={"task": "transcribe"}) #, "language": dest})
+        text = pipe(wav_audio_data, generate_kwargs={"task": "translate", "language": dest})
+        st.markdown(text)
 
-    with st.chat_message("assistant"):
-        
-        if wav_audio_data is not None:
-            pipe = audio_text()
-            # autext = pipe(wav_audio_data, generate_kwargs={"task": "transcribe"}) #, "language": dest})
-            text = pipe(wav_audio_data, generate_kwargs={"task": "translate", "language": dest})
-            st.markdown(text)
-
-with t3: # Images Input
-    # uploads =  
+with t3: # Images Input  
     files = st.file_uploader("Upload images...", type=["jpg", "jpeg", "png"], accept_multiple_files=True, key="file_uploader")
     
-    with st.chat_message("assistant"):
-        if files is not None:
-            st.image(st.session_state["file_uploader"])
-            
-            string, letter = "", ""
-            m = loading()
-            
-            for i in files:
-                img = np.array(Image.open(i))
-                # resized = transform.resize(img, (64, 64, 3))
-                rescaled = img/255
-                # reshaped = np.reshape(rescaled, (64, 64, 3))
-                img = np.expand_dims(rescaled, 2)
-                n2  = np.resize(img, (7, 64, 64, 3))
-                # img = np.expand_dims(n2,0)
-                # img.shape[2] = 3
-                # expnaded = np.expand_dims(img, 0)
-                
-                yhat = m(n2)
+    # st.set_option('deprecation.showfileUploaderEncoding', False)
 
-                st.markdown(yhat)
-            # st.markdown(st.session_state["file_uploader"])
-            # st.write(translated_text)
+    if files is not None:
+        st.image(st.session_state["file_uploader"])
         
+        string, letter = "", ""
+
+        # with st.spinner('Loading Model ...'):
+        #     model = run_model(src)
+
+        
+        #     for image in files:
+
+        #         image = Image.open(image)
+        #         resized_image = image.resize((244, 244), Image.ANTIALIAS)
+                
+        #         predictions = import_and_predict(image, model)
+                
+        #         score = score = tf.nn.softmax(predictions[0])
+
+        #         predicted_class_index = np.argmax(score)
+        #         predicted_class_name = labels[predicted_class_index]
+                
+        #         st.write(f"""## Predicted Letter : {predicted_class_name}""")
+    
 
